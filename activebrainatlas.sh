028f7eec35ea83c2eb7ce5056c7e42abd5bd798d
drinehart <drinehart@ucsd.edu>
2022-03-01 15:31:38 -0800

    changed bash script from symbolic link to actual content (to capture in git)

--- activebrainatlas.sh
+++ activebrainatlas.sh
@@ -1,1 +1,16 @@
-/data/selenium/activebrainatlas.sh
+#!/bin/bash
+
+#CREATE VIRTUAL FRAME BUFFER (XSERVER ON DISPLAY 99)
+nohup /usr/bin/Xvfb :99 -ac -screen 0 1024x768x8 > /tmp/nohup.out 2>&1 &
+
+#SET DEFAULT DISPLAY (REQUIRED IF RUN FROM CRONTAB)
+export DISPLAY=:99
+
+cd /data/selenium/ 
+/data/selenium/venv3.10/bin/python3 /data/selenium/main.py
+
+echo 'SCRIPT COMPLETE'
+
+chmod a+r /mnt/webdev/*.log
+
+
